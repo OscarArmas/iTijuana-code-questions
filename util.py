9bdad0142fbe32b61cd572ef5ae5979f12f57d3f
OscarArmas <saga_cth@hotmail.com>
2021-10-25 00:40:31 -0500

    fix typo

--- util.py
+++ util.py
@@ -2,6 +2,6 @@ import csv
 
 def read_csv(filename, **fmtparams):
     with open(filename, 'r') as csv_file:
-        reader = csv.reader(csv_file, dialect, **fmtparams)
+        reader = csv.reader(csv_file, **fmtparams)
         for row in reader:
             yield row
